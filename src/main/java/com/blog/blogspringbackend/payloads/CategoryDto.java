fc8df9a433fe69e57c32253f54208f812de8f2d5
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 22:41:23 +0530

    Add: Category DTO validations

--- src/main/java/com/blog/blogspringbackend/payloads/CategoryDto.java
+++ src/main/java/com/blog/blogspringbackend/payloads/CategoryDto.java
@@ -1,5 +1,8 @@
 package com.blog.blogspringbackend.payloads;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +11,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class CategoryDto {
+    
+    @NotNull
     private Integer id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "title length should be 3-10 char long")
     private String title;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 10, max = 100, message = "description should be 10-100 chars long")
     private String description;
 }
