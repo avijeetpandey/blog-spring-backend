342d4643a16bab797ad7a41625d79d0b22ee308c
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 21:10:07 +0530

    Add: Model mapper

--- src/main/java/com/blog/blogspringbackend/BlogSpringBackendApplication.java
+++ src/main/java/com/blog/blogspringbackend/BlogSpringBackendApplication.java
@@ -1,12 +1,19 @@
 package com.blog.blogspringbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogSpringBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogSpringBackendApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
