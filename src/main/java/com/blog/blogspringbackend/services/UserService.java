7f32bd66eac96fb0ad7ff3e8d3e0af1371cddc62
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 19:05:52 +0530

    Add: User service interface

--- src/main/java/com/blog/blogspringbackend/services/UserService.java
+++ src/main/java/com/blog/blogspringbackend/services/UserService.java
@@ -1,4 +1,12 @@
 package com.blog.blogspringbackend.services;
 
+import com.blog.blogspringbackend.payloads.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto createUser(UserDto user);
+    UserDto updateUser(UserDto user, Integer userId);
+    UserDto getUserById(Integer userId);
+    List<UserDto> getAllUsers();
 }
