194a0a489353df55a4460304e390fe97b3bcab83
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 19:54:32 +0530

    Add: API mapping to add new user

---
+++ src/main/java/com/blog/blogspringbackend/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.blog.blogspringbackend.controllers;
+
+import com.blog.blogspringbackend.payloads.UserDto;
+import com.blog.blogspringbackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Mapping to create a user
+    @PostMapping("/add")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto createdUser = this.userService.createUser(userDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    // Mapping to delete a user
+
+    // Mapping to get a user
+
+    // Mapping to update a user
+
+}
--- src/main/java/com/blog/blogspringbackend/entity/User.java
+++ src/main/java/com/blog/blogspringbackend/entity/User.java
@@ -18,6 +18,7 @@ public class User {
     @Column(name = "user_name", nullable = false, length = 100)
     private String name;
 
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
     private String about;
