6301796ec507398ae8f643f539682f43e8106d96
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-22 17:28:40 +0530

    Add: Initial code for the blog backend

---
+++ src/main/java/com/blog/blogspringbackend/BlogSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/blogspringbackend/BlogSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
