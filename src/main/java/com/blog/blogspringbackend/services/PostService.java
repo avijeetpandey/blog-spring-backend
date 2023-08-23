9fc283662e2d47063cd701fd92772df6f508901d
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-23 12:21:46 +0530

    Add: Post servcie and PostService Impl class

--- src/main/java/com/blog/blogspringbackend/services/PostService.java
+++ src/main/java/com/blog/blogspringbackend/services/PostService.java
@@ -1,7 +1,17 @@
 package com.blog.blogspringbackend.services;
 
+import com.blog.blogspringbackend.entity.Post;
 import com.blog.blogspringbackend.payloads.PostDto;
 
-public interface PostService {
+import java.util.List;
 
+public interface PostService {
+    Post createPost(PostDto postDto);
+    Post updatePost(PostDto postDto, Integer postId);
+    void deletePost(Integer postId);
+    List<Post> getAllPosts();
+    Post getPostById(Integer postId);
+    List<Post> getPostByCategory(Integer categoryId);
+    List<Post> getPostByUser(Integer userId);
+    List<Post> searchPost(String keyword);
 }
--- src/main/java/com/blog/blogspringbackend/services/impl/PostServiceImpl.java
+++ src/main/java/com/blog/blogspringbackend/services/impl/PostServiceImpl.java
@@ -1,6 +1,49 @@
 package com.blog.blogspringbackend.services.impl;
 
+import com.blog.blogspringbackend.entity.Post;
+import com.blog.blogspringbackend.payloads.PostDto;
 import com.blog.blogspringbackend.services.PostService;
 
+import java.util.List;
+
 public class PostServiceImpl implements PostService {
+    @Override
+    public Post createPost(PostDto postDto) {
+        return null;
+    }
+
+    @Override
+    public Post updatePost(PostDto postDto, Integer postId) {
+        return null;
+    }
+
+    @Override
+    public void deletePost(Integer postId) {
+
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public Post getPostById(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public List<Post> getPostByCategory(Integer categoryId) {
+        return null;
+    }
+
+    @Override
+    public List<Post> getPostByUser(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<Post> searchPost(String keyword) {
+        return null;
+    }
 }
