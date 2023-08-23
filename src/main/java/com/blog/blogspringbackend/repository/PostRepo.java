dcc7f865d5b16b62d83927db28d061a03e6482bb
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-23 12:10:52 +0530

    Fixed generic

--- src/main/java/com/blog/blogspringbackend/repository/PostRepo.java
+++ src/main/java/com/blog/blogspringbackend/repository/PostRepo.java
@@ -3,12 +3,11 @@ package com.blog.blogspringbackend.repository;
 import com.blog.blogspringbackend.entity.Category;
 import com.blog.blogspringbackend.entity.Post;
 import com.blog.blogspringbackend.entity.User;
-import com.blog.blogspringbackend.payloads.PostDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostRepo extends JpaRepository<PostDto, Integer> {
+public interface PostRepo extends JpaRepository<Post, Integer> {
     List<Post> findByUser(User user);
     List<Post> findByCategory(Category category);
 }
